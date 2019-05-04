2e14aeb7dbd211bd7f4b66b22704faea70f1c0dc
Miroslav Poledňák <miroslavpolednak@MiroslavMac.local>
2019-05-04 19:32:50 +0200

    text was changed

--- raiseException.py
+++ raiseException.py
@@ -2,7 +2,7 @@
 # from git hub
 try:
 
-    raise ValueError("exp was raise")
+    raise ValueError("exp was raise from clone repo")
     print("Hello world")
 except Exception as ex:
     print(ex)
